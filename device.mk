399b5163f919aff17b3cd7e36526826a88a66598
dlwlrma123 <alexfinhart@gmail.com>
2022-07-11 05:22:41 +0800

    kebab: Inherit dalvik heap config for 8GB ram devices
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 
 # Additional native libraries
 PRODUCT_COPY_FILES += \
